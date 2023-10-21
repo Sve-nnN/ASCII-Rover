5650363abf4c33ccea93560781e57d4647273c14
Juan Angulo <juancarlosanguloabud@gmail.com>
2023-10-21 12:10:23 -0500

    Creada la interfaz, necesita ser pulida la pantalla de bienvenida y la animacion de lluvia

--- Source.cpp
+++ Source.cpp
@@ -25,7 +25,52 @@ using namespace System;
 //	DESARROLLO DE MISION
 //	FIN DE MISION
 
+void lluvia() {
+    const int MAXGOTAS = 201;
+    const int GOTAS = 10;
 
+    bool caidas= rand() % 501 < GOTAS;
+
+
+    Random r;
+    srand(time(NULL));
+    int* x = new int[MAXGOTAS];
+    int* y = new int[MAXGOTAS];
+    //char* car = new char[MAXGOTAS];
+    int num_gotas = 0;
+    int dy = 1;
+    for (;;)//while(true)
+    {
+        for (int i = 1; i <= 80; i++) {//el margen
+            if (caidas && num_gotas < MAXGOTAS) {
+                x[num_gotas] = i;//posición en x
+                y[num_gotas] = 10;//posición en y
+                ++num_gotas;//num_gotas++
+            }
+        }
+        system("cls");
+        for (int i = 0; i < num_gotas; i++) {
+            if (y[i] < 29) {
+                Console::SetCursorPosition(x[i], y[i]);
+                Console::ForegroundColor = ConsoleColor(rand() % 16);
+                cout << "*";
+                y[i] += dy;
+            }
+            else {
+                --num_gotas;
+                x[i] = x[num_gotas];
+                y[i] = y[num_gotas];
+            }
+        }
+        _sleep(200);
+    }
+    delete[]x;
+    delete[] y;
+    _getch();
+}
+void creditos() {
+
+}
 void bienvenida() {
 	int x = 40, y = 30;
 	system("cls");
@@ -33,6 +78,7 @@ void bienvenida() {
 	string bienvenido = "Bienvenido a Mars Rover";
 	string presiona = "Presiona una tecla para comenzar";
 	int velocidad = 1;
+    lluvia();
 	Console::SetCursorPosition(x,y);
 	for (int i = 0; i < bienvenido.length(); i++) {
 		cout << bienvenido[i];
@@ -46,8 +92,60 @@ void bienvenida() {
 	_getch();
 }
 
+void interfaz() {
+    int alto = 40;
+    int largo = 80;
+    //dibujar cuadrado inicial
+    for (int i = 1; i <= alto; i++) {
+        for (int j = 1; j <= largo; j++) {
+            if (i == 1 || i == alto)cout << "_";
+            else if(j == 1 || j == largo)cout << "|";
+            else cout << " ";
+        }
+        cout << endl;
+    }
+    //primera linea horizontal
+    Console::SetCursorPosition(0, 34);
+    for (int i = 0; i <= largo; i++)cout << "_";
+
+    //lineas horizontales
+    int x = 20, y = 35;
+    for (int i = 1; i < 6; i++) {
+        Console::SetCursorPosition(x, y);
+        cout << "|";
+        y += 1;
+    }    
+     x =40;
+     y = 35;
+    for (int i = 1; i < 6; i++) {
+        Console::SetCursorPosition(x, y);
+        cout << "|";
+        y += 1;
+    }
+    x=60;
+    y = 35;
+    for (int i = 1; i < 6; i++) {
+        Console::SetCursorPosition(x, y);
+        cout << "|";
+        y += 1;
+    }
+
+    //datos para mostrar:
+    Console::SetCursorPosition(1, 36); cout << "Enviado";
+    Console::SetCursorPosition(1, 37); cout << "///////////////////";
+
+    Console::SetCursorPosition(1, 38); cout << "Recibido";
+
+    Console::SetCursorPosition(22, 37); cout << "Velodidad";
+    Console::SetCursorPosition(42, 37); cout << "Altura";
+    Console::SetCursorPosition(62, 37); cout << "Tiempo";
+}
+
+
 int main() {
-	Console::SetWindowSize(80, 40);
-	bienvenida();
+    interfaz();
+    
+    Console::SetCursorPosition(81, 41);
+    system("pause");
 	
 }

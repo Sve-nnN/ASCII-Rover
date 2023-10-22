6b9783dd35fa13bf611ec34e601bd04a792472e7
Juan Angulo <juancarlosanguloabud@gmail.com>
2023-10-22 13:13:41 -0500

    Implementado lluvia de meteoritos. Falta implementar colision, retorno de muestra a la tierra. colores aleatorios en meteoritos

--- Source.cpp
+++ Source.cpp
@@ -1,384 +1,365 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <conio.h>
 #include<Windows.h>
-#define MAXGOTAS 501
-#define GOTAS 5
+
+#define MAXGOTAS 201
+#define GOTAS 10
 
 using namespace std;
 using namespace System;
 
 //REQUISITOS:
-
 //ROVER
-
 //METEORITOS
-
 //MEDIO DE TRANSPORTE DEL ROVER
-
 //ENTORNO DEL VIDEOJUEGO
-
 //PANEL DE CONTROL
-
 //DINAMICA:
-//	PANTALLA DE BIENVENIDA
-//	PANTALLA DE CREDITOS
-//	PANTALLA DE INSTRUCCIONES
-//	NOMBRES DE LOS PERSONAJES
-//	INICIO DE MISION
-//	DESARROLLO DE MISION
-//	FIN DE MISION
+//  PANTALLA DE BIENVENIDA
+//  PANTALLA DE CREDITOS
+//  PANTALLA DE INSTRUCCIONES
+//  NOMBRES DE LOS PERSONAJES
+//  INICIO DE MISION
+//  DESARROLLO DE MISION
+//  FIN DE MISION
 
-/*
-Texto Negro: FOREGROUND_BLACK
-Texto Azul: FOREGROUND_BLUE
-Texto Verde: FOREGROUND_GREEN
-Texto Cyan (Azul claro/verde): FOREGROUND_CYAN
-Texto Rojo: FOREGROUND_RED
-Texto Magenta (Rojo claro/azul): FOREGROUND_MAGENTA
-Texto Amarillo: FOREGROUND_YELLOW
-Texto Blanco (Gris claro): FOREGROUND_WHITE
+void cambiarColorAmarillo() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el identificador de la consola
+    int textAndBackgroundColor = (FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY); // Código de color para texto en amarillo
 
-Fondo Negro: BACKGROUND_BLACK
-Fondo Azul: BACKGROUND_BLUE
-Fondo Verde: BACKGROUND_GREEN
-Fondo Cyan: BACKGROUND_CYAN
-Fondo Rojo: BACKGROUND_RED
-Fondo Magenta: BACKGROUND_MAGENTA
-Fondo Amarillo: BACKGROUND_YELLOW
-Fondo Blanco: BACKGROUND_WHITE
-*/
+    SetConsoleTextAttribute(hConsole, textAndBackgroundColor); // Establecer el color del texto y el fondo
+}
+void cambiarColor(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el identificador de la consola
+    int textAndBackgroundColor = (color); // Código de color para texto en amarillo
+
+    SetConsoleTextAttribute(hConsole, textAndBackgroundColor); // Establecer el color del texto y el fondo
+}
+void reiniciarColores() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el identificador de la consola
+    SetConsoleTextAttribute(hConsole, 7); // 7 es el código de color predeterminado para texto blanco sobre fondo negro
+
+}
 void ubica(int x, int y)
 {
-	Console::SetCursorPosition(x, y);
+    Console::SetCursorPosition(x, y);
 }
 
 void limpiarConsola() {
-	system("cls");
+    system("cls");
 }
-bool caidas() {
-	return rand() % 501 < GOTAS;
+
+bool caidas(int probabilidad) {
+    return rand() % 501 < probabilidad;
 }
+
 void salir() {
-	exit(0);
+    exit(0);
 }
-void cambiarColorAmarillo() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el identificador de la consola
-	int textAndBackgroundColor = (FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY); // Código de color para texto en amarillo
 
-	SetConsoleTextAttribute(hConsole, textAndBackgroundColor); // Establecer el color del texto y el fondo
-}
-void cambiarColor(int color) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el identificador de la consola
-	int textAndBackgroundColor = (color); // Código de color para texto en amarillo
-
-	SetConsoleTextAttribute(hConsole, textAndBackgroundColor); // Establecer el color del texto y el fondo
-}
-void reiniciarColores() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el identificador de la consola
-	SetConsoleTextAttribute(hConsole, 7); // 7 es el código de color predeterminado para texto blanco sobre fondo negro
-
-}
-void lluvia() {
-
-}
 void bienvenida() {
-	int x = 40, y = 20;
-	system("cls");
-	/*cambiarColorAmarillo();*/
-	string bienvenido = "Bienvenido a Mars Rover";
-	string presiona = "Presiona una tecla para comenzar";
-	int velocidad = 1;
-	Console::SetCursorPosition(x, y);
-	for (int i = 0; i < bienvenido.length(); i++) {
-		cout << bienvenido[i];
-		_sleep(velocidad);
-	}
-	Console::SetCursorPosition(x, y + 2);
-	for (int i = 0; i < presiona.length(); i++) {
-		cout << presiona[i];
-		_sleep(velocidad);
-	}
-	_getch();
+    int x = 40, y = 20;
+    system("cls");
+    cambiarColorAmarillo();
+    string bienvenido = "Bienvenido a Mars Rover";
+    string presiona = "Presiona una tecla para comenzar";
+    int velocidad = 1;
+    Console::SetCursorPosition(x, y);
+    for (int i = 0; i < bienvenido.length(); i++) {
+        cout << bienvenido[i];
+        _sleep(velocidad);
+    }
+    Console::SetCursorPosition(x, y + 2);
+    for (int i = 0; i < presiona.length(); i++) {
+        cout << presiona[i];
+        _sleep(velocidad);
+    }
+    reiniciarColores();
+    _getch();
 }
-void menuOpciones();
-void creditos() {
-	limpiarConsola();
-	cout << "DISEÑO DE INTERFACES\t JUAN ANGULO\nDISEÑO FIGURAS ASCII\tMARCELO MILLA\nDESARROLLO DE FUNCIONALIDADES\tGIOVANNI OCAÑA\nDESARROLLO DE MECANICAS\tTIMOTHY DOCUMET";
-	cout << "\n\nPresiona una tecla para volver al menu de inicio";
-	_getch();
-	menuOpciones();
-}
-void tutorial() {
-	limpiarConsola();
-	cout << "¿Como jugar?" << endl
-		<< "En este juego, tomas el papel del Rover Perserverance,\nel cual debe tener un aterrizaje exitoso en la superficie marciana\npara poder enviarles muestras del planeta a la tierra.";
-	cout << "\n\nTu objetivo es descender con exito al planeta rojo\nsin chocar con los meteoritos, ya que estos impediran tu aterrizaje\n";
-	cout << "\n\nPresiona una tecla para volver al menu de inicio";
-	_getch();
-	menuOpciones();
-}
-int menu() {
-	limpiarConsola();
-	int opcion;
-	cout << "MENU"
-		<< endl
-		<< "[1] INSTRUCCIONES"
-		<< endl
-		<< "[2] JUGAR"
-		<< endl
-		<< "[3] CREDITOS"
-		<< endl
-		<< "[4] SALIR" << endl;
-	do {
-		cout << "Selecciona una opcion: "; cin >> int(opcion);
-	} while (opcion < 1 || opcion>4);
-	return opcion;
 
+void menuOpciones();
+
+void creditos() {
+    limpiarConsola();
+    cout << "DISEÑO DE INTERFACES\t JUAN ANGULO\nDISEÑO FIGURAS ASCII\tMARCELO MILLA\nDESARROLLO DE FUNCIONALIDADES\tGIOVANNI OCAÑA\nDESARROLLO DE MECANICAS\tTIMOTHY DOCUMET";
+    cout << "\n\nPresiona una tecla para volver al menu de inicio";
+    _getch();
+    menuOpciones();
+}
+
+void tutorial() {
+    limpiarConsola();
+    cout << "¿Como jugar?" << endl
+        << "En este juego, tomas el papel del Rover Perserverance,\nel cual debe tener un aterrizaje exitoso en la superficie marciana\npara poder enviarles muestras del planeta a la tierra.";
+    cout << "\n\nTu objetivo es descender con exito al planeta rojo\nsin chocar con los meteoritos, ya que estos impediran tu aterrizaje\n";
+    cout << "\n\nPresiona una tecla para volver al menu de inicio";
+    _getch();
+    menuOpciones();
+}
+
+int menu() {
+    limpiarConsola();
+    int opcion;
+    cout << "MENU"
+        << endl
+        << "[1] INSTRUCCIONES"
+        << endl
+        << "[2] JUGAR"
+        << endl
+        << "[3] CREDITOS"
+        << endl
+        << "[4] SALIR" << endl;
+    do {
+        cout << "Selecciona una opcion: "; cin >> int(opcion);
+    } while (opcion < 1 || opcion>4);
+    return opcion;
 }
 
 void interfaz() {
-	limpiarConsola();
-	Console::SetCursorPosition(0, 0);
-	int alto = 40;
-	int largo = 80;
-	//dibujar cuadrado inicial
-	for (int i = 1; i <= alto; i++) {
-		for (int j = 1; j <= largo; j++) {
-			if (i == 1 || i == alto)cout << "~";
-			else if (j == 1 || j == largo)cout << "|";
-			else cout << " ";
-		}
-		cout << endl;
-	}
-	//primera linea horizontal
-	Console::SetCursorPosition(1, 34);
-	for (int i = 0; i <= largo - 3; i++)cout << "_";
+    limpiarConsola();
+    Console::SetCursorPosition(0, 0);
+    int alto = 40;
+    int largo = 80;
+    for (int i = 1; i <= alto; i++) {
+        for (int j = 1; j <= largo; j++) {
+            if (i == 1 || i == alto) cout << "~";
+            else if (j == 1 || j == largo) cout << "|";
+            else cout << " ";
+        }
+        cout << endl;
+    }
+    Console::SetCursorPosition(1, 34);
+    for (int i = 0; i <= largo - 3; i++) cout << "_";
 
-	//lineas horizontales
-	int x = 20, y = 35;
-	for (int i = 1; i < 5; i++) {
-		Console::SetCursorPosition(x, y);
-		cout << "|";
-		y += 1;
-	}
-	x = 40;
-	y = 35;
-	for (int i = 1; i < 5; i++) {
-		Console::SetCursorPosition(x, y);
-		cout << "|";
-		y += 1;
-	}
-	x = 60;
-	y = 35;
-	for (int i = 1; i < 5; i++) {
-		Console::SetCursorPosition(x, y);
-		cout << "|";
-		y += 1;
-	}
+    //lineas horizontales
+    int x = 20, y = 35;
+    for (int i = 1; i < 5; i++) {
+        Console::SetCursorPosition(x, y);
+        cout << "|";
+        y += 1;
+    }
+    x = 40;
+    y = 35;
+    for (int i = 1; i < 5; i++) {
+        Console::SetCursorPosition(x, y);
+        cout << "|";
+        y += 1;
+    }
+    x = 60;
+    y = 35;
+    for (int i = 1; i < 5; i++) {
+        Console::SetCursorPosition(x, y);
+        cout << "|";
+        y += 1;
+    }
 
-	//datos para mostrar:
-	Console::SetCursorPosition(1, 36); cout << "Enviado";
-	Console::SetCursorPosition(1, 37); cout << "~~~~~~~~~~~~~~~~~~~";
 
-	Console::SetCursorPosition(1, 38); cout << "Recibido";
-
-	Console::SetCursorPosition(22, 37); cout << "Velodidad";
-	Console::SetCursorPosition(42, 37); cout << "Altura";
-	Console::SetCursorPosition(62, 37); cout << "Tiempo";
 }
+
 void generarCrater() {
-	Random r;
-	Console::SetCursorPosition(0, 0);
-	int nCrateres = r.Next(5, 10);
-	int x;
-	int y;
-	for (int i = 0; i < nCrateres; i++) {
-		x = r.Next(2, 78);//1 + rand() % 79 - 1;
-		y = r.Next(32, 34);//31 + rand() % 35;
-		Console::SetCursorPosition(x, y);
-		cout << char(r.Next('\'', '0' + 1));
-	}
-
+    Random r;
+    Console::SetCursorPosition(0, 0);
+    int nCrateres = r.Next(5, 10);
+    int x;
+    int y;
+    for (int i = 0; i < nCrateres; i++) {
+        x = r.Next(2, 78);
+        y = r.Next(32, 34);
+        Console::SetCursorPosition(x, y);
+        cout << char(r.Next('\'', '0' + 1));
+    }
 }
+
 void generarMarte() {
-	Console::SetCursorPosition(1, 30);
-	cambiarColor(4);
-	for (int i = 0; i <= 77; i++)cout << "~";
-	generarCrater();
-
+    Console::SetCursorPosition(1, 30);
+    cambiarColor(4);
+    for (int i = 0; i <= 77; i++) cout << "~";
+    generarCrater();
 }
+
 int generarMeteoritoX() {
-	int generar = 5 + rand() % 77;
-	return generar;
+    int generar = 5 + rand() % 77;
+    return generar;
 }
+
 int generarMeteoritoY() {
-	int generar = 5 + rand() % 29 - 5;
-	return generar;
-}
-void movimientoMeteorito(int& x, int& y) {
-
-}
-void borrar_rover(int x, int y)
-{
-	ubica(x, y);     cout << "    ";
-	ubica(x, y + 1); cout << "    ";
-
+    int generar = 5 + rand() % 29 - 5;
+    return generar;
 }
 
-
-void dibujar_rover(int x, int y)
-{
-	ubica(x, y);     cout << "***";
-	ubica(x, y + 1); cout << "***";
-
+void dibujarMeteoritoEstatico(int x, int y) {
+    Console::SetCursorPosition(x, y);
+    cout << "*";
 }
-void borrar_transporte(int xt, int yt)
-{
-	ubica(xt, yt);     cout << "     ";
 
+void dibujarMeteoritoMovil(int& x, int& y) {
+    Random r;
+    //borro
+    Console::SetCursorPosition(x, y);
+    cout << " ";
+    //muevo
+    y++;
+    //imprimo
+    Console::SetCursorPosition(x, y);
+    cout << char(r.Next('*', '.' + 1));
 
 }
 
-
-void dibujar_transporte(int xt, int yt)
-{
-	ubica(xt, yt); cout << "O---O";
-
-
+void borrarRover(int x, int y) {
+    ubica(x, y);     cout << "    ";
+    ubica(x, y + 1); cout << "    ";
 }
+
+void dibujarRover(int x, int y) {
+    ubica(x, y);     cout << "/ \\";
+    ubica(x, y + 1); cout << "o=o";
+}
+
+void borrarTransporte(int xt, int yt) {
+    ubica(xt, yt);     cout << "     ";
+}
+
+void dibujarTransporte(int xt, int yt) {
+    ubica(xt, yt); cout << "O---O";
+}
+
 void juego() {
-	int x, y;
-	int xt = 3, yt = 1;
-	Random r;
-	srand(time(NULL));
+    Random r;
+    int enviado = 0, recibido = 0, altura = 200, velocidad = r.Next(1, 5), tiempo=altura/velocidad, puntaje=0;
+    int puntajeColision = 50;
+    int puntajeMuestra = 150;
+    int puntajeAterrizaje=200;
+    int x, y;
+    int xt = 3, yt = 1;
+    int nMeteoritosEstaticos = r.Next(7, 16);
+    int nMeteoritosMoviles = r.Next(3, 6);
+    //array para la posicion de los meteoritos estaticos
+    int* meteoritoEstaticoX = new int[nMeteoritosEstaticos];
+    int* meteoritoEstaticoY = new int[nMeteoritosEstaticos];
+    //array para meteoritos moviles
+    int* meteoritoMovilX = new int[nMeteoritosMoviles];
+    int* meteoritoMovilY = new int[nMeteoritosMoviles];
+    int dMeteorito = 1;
+    int dyMeteorito = 1;
+    bool colision = false;
+    interfaz();
+    //imprime los meteoritos estaticos
+    for (int i = 0; i < nMeteoritosEstaticos; i++) {
+        meteoritoEstaticoX[i] = generarMeteoritoX();
+        meteoritoEstaticoY[i] = generarMeteoritoY();
+        dibujarMeteoritoEstatico(meteoritoEstaticoX[i], meteoritoEstaticoY[i]);
+    }
+    //imprime los meteoritos dinamicos
+    for (int i = 0; i < nMeteoritosMoviles; i++) {
+        meteoritoMovilX[i] = generarMeteoritoX();
+        meteoritoMovilY[i] = generarMeteoritoY();
+        dibujarMeteoritoMovil(meteoritoMovilX[i], meteoritoMovilY[i]);
+    }
+    generarMarte();
+    Console::SetCursorPosition(1, 1);
+    int dx = 1, dy = 1;
+    bool transporteDesplegado = false;
+    bool caidaLibre = false;
+    while (1) {
+        dibujarTransporte(xt, yt);
+        if (_kbhit()) {
+            //borrar
+            tiempo--;
+            borrarTransporte(xt, yt);
+            //mover
+            char tecla = _getch();
+            if (transporteDesplegado == false) {
+                if (tecla == char(77)) xt++;
+                if (tecla == char(75)) xt--;
+                if (tecla == char(32)) { 
+                    x = xt; 
+                    y = yt + 1; 
+                    transporteDesplegado = true; 
+                    caidaLibre = true;
+                }
+            }
+            if (transporteDesplegado == true) {
+                //borrar
+                borrarRover(x, y);
+                if (caidaLibre == true) {
+                    //velocidad en caida libre
+                    y += dy;
+                    //modifico altura
+                    altura -= velocidad;
+                    if (tecla == char(77) && x + 4 < 77) x++;
+                    if (tecla == char(75) && x > 3) x--;
+                    if (y == 29) {
+                        caidaLibre = false;
+                        puntaje += puntajeAterrizaje;
+                    }
+                }
+                if (caidaLibre == false) {
+                    if (tecla == char(77) && x + 4 < 79) x++;
+                    if (tecla == char(75) && x > 3) x--;
+                }
 
-	int nMeteoritosEstaticos = r.Next(7, 16);
-	int nMeteoritosMoviles = r.Next(3, 6);
-	//array para la posicion de los meteoritos estaticos
-	int* meteoritoEstaticoX = new int[nMeteoritosEstaticos];
-	int* meteoritoEstaticoY = new int[nMeteoritosEstaticos];
-	//array para meteoritos moviles
-	int* meteoritoMovilX = new int[nMeteoritosMoviles];
-	int* meteoritoMovilY = new int[nMeteoritosMoviles];
-	int dMeteorito = 1;
-	//variables para la lluvia de meteoritos multicolor:
-	int* xGota = new int[MAXGOTAS];
-	int* yGota = new int[MAXGOTAS];
-	//char* car = new char[MAXGOTAS];
-	int num_gotas = 0;
-	int dy = 1;
-	for (int i = 1; i <= 77; i++) {//el margen
-		if (caidas() && num_gotas < MAXGOTAS) {
-			xGota[num_gotas] = i;//posición en x
-			yGota[num_gotas] = 10;//posición en y
-			++num_gotas;//num_gotas++
-		}
-	}
-	interfaz();
-	//imprime los meteoritos estaticos
-	for (int i = 0; i < nMeteoritosEstaticos; i++) {
-		meteoritoEstaticoX[i] = generarMeteoritoX();
-		meteoritoEstaticoY[i] = generarMeteoritoY();
-		Console::SetCursorPosition(meteoritoEstaticoX[i], meteoritoEstaticoY[i]);
-		cout << char(r.Next('*', '.' + 1));
-	}
-	//imprime los meteoritos dinamicos
-	for (int i = 0; i < nMeteoritosMoviles; i++) {
-		meteoritoMovilX[i] = generarMeteoritoX();
-		meteoritoMovilY[i] = generarMeteoritoY();
-		Console::SetCursorPosition(meteoritoMovilX[i], meteoritoMovilY[i]);
-		cout << char(244);
-	}
-	generarMarte();
-	Console::SetCursorPosition(1, 1);
-	int d = 0;
-	while (1)
-	{
+                dibujarRover(x, y);
+            }
+            dibujarTransporte(xt, yt);
+        }
+        for (int i = 0; i < nMeteoritosMoviles; i++) {
+            if (meteoritoMovilY[i] < 30) {
+                if (meteoritoMovilY[i] > yt && meteoritoMovilY[i] < yt + 4) {
+                    if (meteoritoMovilX[i] > xt && meteoritoMovilX[i] < xt + 7) {
+                        puntaje -= puntajeColision;
+                    }
+                }
+                dibujarMeteoritoMovil(meteoritoMovilX[i], meteoritoMovilY[i]);
+            }
+            else {
+                meteoritoMovilX[i] = generarMeteoritoX();
+                meteoritoMovilY[i] = generarMeteoritoY();
+            }
+        }
+        //datos para mostrar:
+        Console::SetCursorPosition(1, 36); cout << "Enviado: "<<enviado;
+        Console::SetCursorPosition(1, 37); cout << "~~~~~~~~~~~~~~~~~~~";
+        Console::SetCursorPosition(1, 38); cout << "Recibido: "<<recibido;
+        Console::SetCursorPosition(22, 36); cout << "Velodidad: "<<velocidad;
+        Console::SetCursorPosition(21, 37); cout << "~~~~~~~~~~~~~~~~~~~";
+        Console::SetCursorPosition(22, 38); cout << "Altura: "<<altura;
+        Console::SetCursorPosition(42, 38); cout << "Puntos: " << puntaje;
+        Console::SetCursorPosition(62, 37); cout << "Tiempo: "<<tiempo;
+        _sleep(100);
+    }
 
-		if (_kbhit())
-		{
-			//borrar
-			borrar_transporte(xt, yt);
+    delete[] meteoritoEstaticoX;
+    delete[] meteoritoEstaticoY;
+    delete[] meteoritoMovilX;
+    delete[] meteoritoMovilY;
 
-			//mover
-			char tecla = _getch();
-			if (d == 0) {
-				if (tecla == char(77)) xt++;
-				if (tecla == char(75)) xt--;
-				if (tecla == char(32)) { x = xt; y = yt + 1; d = 1; }
-			}
-			if (d == 1) {
-
-				//borrar
-				borrar_rover(x, y);
-
-
-
-				if (tecla == char(77) && x + 4 < 79) x++;
-
-				if (tecla == char(75) && x > 3) x--;
-
-				if (tecla == char(72) && y > 2)  y--;
-
-				if (tecla == char(80) && y + 3 < 32) y++;
-
-				dibujar_rover(x, y);
-			}
-			dibujar_transporte(xt, yt);
-			for (int i = 0; i < num_gotas; i++) {
-				if (yGota[i] < 29) {
-					Console::SetCursorPosition(x[i], y[i]);
-					cout << " ";
-					yGota[i] += dy;
-					Console::SetCursorPosition(x[i], y[i]);
-					Console::ForegroundColor = ConsoleColor(rand() % 16);
-					cout << "*";
-				}
-				else if (y[i] == 29) {
-					Console::SetCursorPosition(x[i], y[i]);
-					cout << " ";
-				}
-				else {
-					--num_gotas;
-					xGota[i] = x[num_gotas];
-					yGota[i] = y[num_gotas];
-				}
-			}
-			_sleep(200);
-
-		}
-	}
-	delete[]x;
-	delete[] y;
+    _getch();
 }
 
 void menuOpciones() {
-	int opcion = menu();
-	switch (opcion)
-	{
-	case 1:
-		tutorial();
-		break;
-	case 2:
-		juego();
-		break;
-	case 3:
-		creditos();
-		break;
-	case 4:
-		salir();
-		break;
-	default:
-		break;
-	}
-
+    int opcion = menu();
+    switch (opcion) {
+    case 1:
+        tutorial();
+        break;
+    case 2:
+        juego();
+        break;
+    case 3:
+        creditos();
+        break;
+    case 4:
+        salir();
+        break;
+    default:
+        break;
+    }
 }
 
 int main() {
-
-	bienvenida();
-	menuOpciones();
-	Console::SetCursorPosition(81, 41);
-	_getch();
+    bienvenida();
+    menuOpciones();
+    Console::SetCursorPosition(81, 41);
+    _getch();
 }
